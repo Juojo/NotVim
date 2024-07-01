221cc1163f91cab9f7b0590d2d28c59a63d94f6c
Juojo <minarro.juanmartin@gmail.com>
2024-07-01 16:43:40 -0300

    Cooked terminal mode

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,31 @@
 package org.example;
 
+import java.io.IOException;
+
 import org.example.util.Colors;
 
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println("Hola");
 //        System.out.println("\033[4;44;31mHola\033[0m");
 
-        System.out.println(escArroundString("funcion", Colors.DEFAULT.getColor()));
-        System.out.println("SSH");
+        System.out.println(printCustomString("Hola", Colors.RED.getColor()));
+        
+        int key = 0;
+        
+        while ((int) key != 113) { // 113 == "q"
+			try {
+				key = System.in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println((int) key);
+		}
 
     }
 
-    private static String escArroundString(String content, String color) {
+    private static String printCustomString(String content, String color) {
         return ("\033[" + ";" + ";" + color + "m" + content + "\033[0m");
     }
 }
