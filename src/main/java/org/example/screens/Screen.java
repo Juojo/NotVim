c5d7ceecb2fd964ce0cdab9edb967cefc2b1cd8b
Juojo <minarro.juanmartin@gmail.com>
2024-07-09 17:51:08 -0300

    Special char are now handle in a while loop instead of an if statement

--- src/main/java/org/example/screens/Screen.java
+++ src/main/java/org/example/screens/Screen.java
@@ -25,42 +25,56 @@ public abstract class Screen {
 		int secondChar = 0, thirdChar = 0;
 		firstChar = System.in.read();
 		
+		Mode originalMode;
+		while (firstChar == 27) {
+			originalMode = mode;
+			changeMode(Mode.NORMAL_MODE);
+			
+			secondChar = System.in.read();
+			
+			if (secondChar != 27) { // if 27 the ESC key has been pressed 2 times, restart cycle
+				
+				// Handle special characters and re assign NORMAL MODE if ESC key pressed
+				if (secondChar == 91) {
+					changeMode(originalMode);
+					thirdChar = System.in.read();
+					if (thirdChar == 65) System.out.print("arrow up");
+					if (thirdChar == 66) System.out.print("arrow down");
+					if (thirdChar == 67) System.out.print("arrow right");
+					if (thirdChar == 68) System.out.print("arrow left");
+				} else if (secondChar == 79) {
+					changeMode(originalMode);
+					thirdChar = System.in.read();
+					if (thirdChar == 80) System.out.print("F1");
+					if (thirdChar == 81) System.out.print("F2");
+				}
+				
+				// Handle custom binds if pressed after 27
+				// Enter EX MODE if second char is ":" and if mode is NORMAL MODE
+				if (secondChar == 58 && mode == Mode.NORMAL_MODE) {
+					changeMode(Mode.EX_MODE);
+					System.out.print(returnColorString("Enter command:", Colors.BLUE.getColor()));
+				}
+				
+				firstChar = 0; // set first char to ASCII NULL to avoid print and exit cycle
+			} else {
+				firstChar = 27;
+			}
+		}
+		
+		// Handle custom binds:
+		// In any mode
 		if (firstChar == 113) { // q
 			loop = false;
 		} else if (firstChar == 13 || firstChar == 10) { // Enter
 			System.out.print("\n");
 		}
 		
-		Mode originalMode;
-		if (firstChar == 27) {
-			originalMode = mode;
-			changeMode(Mode.NORMAL_MODE);
-			secondChar = System.in.read();
-			
-			// Handle special characters and assign NORMAL MODE if ESC key pressed
-			if (secondChar == 91) {
-				changeMode(originalMode);
-				thirdChar = System.in.read();
-				if (thirdChar == 65) System.out.print("arrow up");
-				if (thirdChar == 66) System.out.print("arrow down");
-				if (thirdChar == 67) System.out.print("arrow right");
-				if (thirdChar == 68) System.out.print("arrow left");
-			} else if (secondChar == 79) {
-				changeMode(originalMode);
-				thirdChar = System.in.read();
-				if (thirdChar == 80) System.out.print("F1");
-				if (thirdChar == 81) System.out.print("F2");
-			} else {
-				//changeMode(Mode.NORMAL_MODE);
+		// In NORMAL MODE
+		if (mode == Mode.NORMAL_MODE) {
+			if (firstChar == 105) {// i
+				changeMode(Mode.INSERT_MODE);
 			}
-			
-			// Enter EX MODE if second char is ":" and if mode is NORMAL MODE
-			if (secondChar == 58 && mode == Mode.NORMAL_MODE) {
-				changeMode(Mode.EX_MODE);
-				System.out.print(returnColorString("Enter command:", Colors.BLUE.getColor()));
-			}
-			
-			firstChar = 0; // set first char to ASCII NULL to avoid print
 		}
 		
 	}
