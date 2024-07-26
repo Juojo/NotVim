ac74cb6cb8f53cfe6296ed5be9490f247912ffd2
Juojo <minarro.juanmartin@gmail.com>
2024-07-26 19:54:00 -0300

    Remove : char from start of all commands

--- src/main/java/com/juojo/commands/CreateCommandInstance.java
+++ src/main/java/com/juojo/commands/CreateCommandInstance.java
@@ -4,8 +4,8 @@ public class CreateCommandInstance {
 
 	public CreateCommandInstance(String userInput) {
 		
-		if (userInput.equals(":q") || userInput.equals(":quit")) new Quit();
-		//else if (userInput.equals(":w") || userInput.equals(":write")) new Write();
+		if (userInput.equals("q") || userInput.equals("quit")) new Quit();
+		//else if (userInput.equals("w") || userInput.equals("write")) new Write();
 		//else new Alert("Command not found");
 		
 	}
