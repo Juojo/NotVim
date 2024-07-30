13e4828dd06c86e116943b52ef065d200d0696d6
Juojo <minarro.juanmartin@gmail.com>
2024-07-29 22:37:07 -0300

    Add open file logic

--- src/main/java/com/juojo/commands/CreateCommandInstance.java
+++ src/main/java/com/juojo/commands/CreateCommandInstance.java
@@ -8,6 +8,7 @@ public class CreateCommandInstance {
 	public CreateCommandInstance(String userInput) {
 		
 		if (userInput.equals("q") || userInput.equals("quit")) new Quit();
+		else if (userInput.equals("open")) new Open();
 		//else if (userInput.equals("w") || userInput.equals("write")) new Write();
 		else Alerts.COMMAND_NOT_FOUND.newAlert();
 		
--- src/main/java/com/juojo/commands/Open.java
+++ src/main/java/com/juojo/commands/Open.java
@@ -1,5 +1,13 @@
 package com.juojo.commands;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.juojo.screens.Screen;
+import com.juojo.util.Alerts;
+import com.juojo.util.Colors;
+
 public class Open extends Command {
 
 	private static final String name = "Open";
@@ -9,12 +17,34 @@ public class Open extends Command {
 	
 	protected Open() {
 		super(name, desc);
-		path = "./testFile";
 	}
 
 	@Override
 	protected void executeCommand() {
+		path = "./testFile";
+		String data = "";
 		
+		if (Screen.canHandleFiles()) { // Verify that the screen is capable of handling files.
+			try {
+				File file = new File(path);
+				Scanner fileReader = new Scanner(file);
+				
+				while (fileReader.hasNextLine()) {
+					data = fileReader.nextLine();
+					System.out.print(data + "\n\r");
+				}
+				
+				fileReader.close();
+			} catch (FileNotFoundException e) {
+				Alerts.FILE_NOT_FOUND.newAlert();
+			} catch (Exception e) {
+				Alerts.newCustomAlert("Exception not handled", e.toString(), Colors.RED, null);
+				//e.printStackTrace();
+			}
+			
+		} else {
+			Alerts.CANT_OPEN_FILE.newAlert();
+		}
 	}
 
 }
