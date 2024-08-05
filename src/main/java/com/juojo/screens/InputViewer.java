db8a74a19c043d8257b75a8d4ef5c19aa2f0a75f
Juojo <minarro.juanmartin@gmail.com>
2024-08-04 21:10:20 -0300

    Code refactoring

--- src/main/java/com/juojo/screens/InputViewer.java
+++ src/main/java/com/juojo/screens/InputViewer.java
@@ -2,26 +2,34 @@ package com.juojo.screens;
 
 import java.io.IOException;
 
+import com.juojo.util.Alerts;
 import com.juojo.util.Colors;
 import com.juojo.util.Util;
 
 public class InputViewer extends Screen {
 
+	private static boolean canHandleFiles = false;
+	
 	public InputViewer(int row, int col) {
-		super(row, col, false);
+		super(row, col, canHandleFiles);
 		
 		Util.clearScreen();
 		Util.moveCursorHome();
 		
-		// Handle key and print char
-        while (super.getLoop()) { // 113 == "q"
+        while (super.getLoop()) {
 			try {
 				handleKey();
 			} catch (IOException e) {
-				e.printStackTrace();
+				Alerts.newCustomAlert("Error", e.toString(), Colors.RED, null);
+				//e.printStackTrace();
 			}
-			System.out.printf("%s %s %s %d\r\n", Util.returnColorString("Char:", Colors.RED, Colors.DEFAULT), (char) super.charCode, Util.returnColorString("Int:", Colors.RED, Colors.DEFAULT), (int) super.charCode);
+			
+			printChar();
 		}
+	}
+
+	private void printChar() {
+		System.out.printf("%s %s %s %d\r\n", Util.returnColorString("Char:", Colors.RED, Colors.DEFAULT), (char) super.charCode, Util.returnColorString("Int:", Colors.RED, Colors.DEFAULT), (int) super.charCode);
 	}
 
 }
--- src/main/java/com/juojo/screens/TextViewer.java
+++ src/main/java/com/juojo/screens/TextViewer.java
@@ -2,39 +2,47 @@ package com.juojo.screens;
 
 import java.io.IOException;
 
+import com.juojo.util.Alerts;
 import com.juojo.util.Colors;
 import com.juojo.util.Util;
 
 public class TextViewer extends Screen {
 	
+	private static boolean canHandleFiles = true;
+	
 	public TextViewer(int row, int col) {
-		super(row, col, true);	
+		super(row, col, canHandleFiles);
 		Util.clearScreen();
 	
-		// Print initial screen
-		for (int i = 0; i < row-super.getStatusHeight(); i++) {
-			System.out.printf("%s\r\n", Util.returnColorString("~", Colors.BLUE, Colors.DEFAULT));
-		}
-		printStatusBar(false, super.posX, super.posY, super.exPosX);
+		printHomeScreen();
+		super.printStatusBar(false, super.posX, super.posY, super.exPosX);
 		
 		Util.moveCursorHome();
 		
-		// Handle key and print char
         while (super.getLoop()) {
 			try {
-				handleKey();
+				super.handleKey();
 			} catch (IOException e) {
-				e.printStackTrace();
+				Alerts.newCustomAlert("Error", e.toString(), Colors.RED, null);
+				//e.printStackTrace();
 			}
 			
-			if (super.charCode >= 0 && (super.mode == Mode.INSERT_MODE || super.mode == Mode.EX_MODE)) {
-				System.out.print((char) super.charCode);
-				if (super.mode == Mode.INSERT_MODE) super.posX++;
-				if (super.mode == Mode.EX_MODE) super.exPosX++;
-			}
+			printChar();
 		}
 	}
 
-	
-	
+	private void printHomeScreen() {
+		for (int i = 0; i < super.getRow()-super.getStatusHeight(); i++) {
+			System.out.printf("%s\r\n", Util.returnColorString("~", Colors.BLUE, Colors.DEFAULT));
+		}
+	}
+
+	private void printChar() {
+		if (super.charCode >= 0 && (super.mode == Mode.INSERT_MODE || super.mode == Mode.EX_MODE)) {
+			System.out.print((char) super.charCode);
+			if (super.mode == Mode.INSERT_MODE) super.posX++;
+			if (super.mode == Mode.EX_MODE) super.exPosX++;
+		}
+	}
+
 }
