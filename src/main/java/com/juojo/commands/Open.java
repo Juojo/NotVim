4ed0119dd372b05c403aad5920bd874627cbeb5b
Juojo <minarro.juanmartin@gmail.com>
2024-08-06 18:48:08 -0300

    Remove duplicated code from Open

--- src/main/java/com/juojo/commands/Open.java
+++ src/main/java/com/juojo/commands/Open.java
@@ -1,17 +1,8 @@
 package com.juojo.commands;
 
-import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Stream;
 
-import com.juojo.screens.Screen;
-import com.juojo.util.ANSI;
-import com.juojo.util.Alerts;
-import com.juojo.util.Colors;
-import com.juojo.util.Util;
+import com.juojo.screens.Data;
 
 public class Open extends Command {
 
@@ -19,7 +10,6 @@ public class Open extends Command {
 	private static final String desc = "Open test file.";
 	
 	private String[] args;
-	private Path path;
 	
 	protected Open(String[] args) {
 		super(name, desc);
@@ -30,45 +20,16 @@ public class Open extends Command {
 
 	@Override
 	protected void executeCommand() {
-		List<String> data = List.of();
-	
+		Path path;
+		
 		if (args[0] == null) {
 			path = Path.of("testFile");
 		} else {
 			path = Path.of(args[0]);
 		}
 		
-		if (Screen.canHandleFiles()) { // Verify that the screen is capable of handling files.
-			try {
-				Screen.cursor.moveSet(1, 1);
-				
-				Stream<String> stream = Files.lines(path);
-				data = stream.toList();
-				
-				for (int i = 0; i < Screen.getTerminalRow()-Screen.getStatusHeight(); i++) {
-					if (i >= data.size()) {
-						System.out.print(Util.returnColorString("~", Colors.BLUE, Colors.DEFAULT));
-					} else {
-						System.out.print(data.get(i));
-					}
-					ANSI.deleteEndOfRow();
-					System.out.print("\r\n");
-					
-				}
-				
-				Screen.cursor.moveSet(1, 1);
-				
-			} catch (FileNotFoundException e) {
-				Alerts.FILE_NOT_FOUND.newAlert();
-			} catch (NoSuchFileException e) {
-				Alerts.FILE_NOT_FOUND.newAlert();
-			} catch (Exception e) {
-				Alerts.newCustomAlert("Error", e.toString(), Colors.RED, null);
-				//e.printStackTrace();
-			}
-		} else {
-			Alerts.CANT_OPEN_FILE.newAlert();
-		}
+		Data data = new Data();
+		data.print(path);
 	}
 
 }
